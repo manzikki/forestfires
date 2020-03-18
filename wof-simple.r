fe8772930cc76a92b7754b9d34f05b075f78405e
Marko <manzikki@gmail.com>
2020-03-18 08:11:23 +0000

    multiple variables for bar graphs

--- wof-simple.r
+++ wof-simple.r
@@ -22,8 +22,8 @@ country <- raster::getData(name = "GADM", country = "Thailand", level = 0)
 gfas_data_mean <- brick("gfas_0001_cfire_climatology_2003_2018.nc",
                         varname = "gfas_data_mean")
 
-# Load current emissions
-current_emissions <- brick(woffile)
+# Load current emissions: Wildfire Overall flux
+current_emissions <- brick(woffile, varname="cfire")
 labels_current <- substr(names(current_emissions), 7, 11)
 aoi <- as(extent(gfas_data_mean), "SpatialPolygons")
 current_area <- raster::area(current_emissions) * 1000000 # in m2
@@ -43,8 +43,61 @@ times <- convertDateNcdf2R(time, units = "hours", origin = as.POSIXct("1900-01-0
 times_no <- str_replace(times[20], "UTC", "")
 times_no <- str_replace(times_no, "-20", "")
 
-jpeg("wof.jpg")
+jpeg("wof-month.jpg")
 barplot(current_sum, names.arg=idx, main=paste("Wildfire Overall Flux of Burnt Carbon, Thailand", times_no))
+
+
+# Load current emissions: CO2
+current_emissions <- brick(woffile, varname="co2fire")
+labels_current <- substr(names(current_emissions), 7, 11)
+aoi <- as(extent(gfas_data_mean), "SpatialPolygons")
+current_area <- raster::area(current_emissions) * 1000000 # in m2
+current <- raster::mask(current_emissions * current_area, aoi)
+current <- mask(current, country)
+
+# Find indices in common
+idx <- labels_current
+
+# Compute sum over the area
+current_sum <- cellStats(current, sum) * 86400 * 1E-9
+
+jpeg("co2-month.jpg")
+barplot(current_sum, names.arg=idx, main=paste("Wildfire flux of Carbon Dioxide, Thailand", times_no))
+
+# Load current emissions: CO
+current_emissions <- brick(woffile, varname="cofire")
+labels_current <- substr(names(current_emissions), 7, 11)
+aoi <- as(extent(gfas_data_mean), "SpatialPolygons")
+current_area <- raster::area(current_emissions) * 1000000 # in m2
+current <- raster::mask(current_emissions * current_area, aoi)
+current <- mask(current, country)
+
+# Find indices in common
+idx <- labels_current
+
+# Compute sum over the area
+current_sum <- cellStats(current, sum) * 86400 * 1E-9
+
+jpeg("co-month.jpg")
+barplot(current_sum, names.arg=idx, main=paste("Wildfire flux of Carbon Monoxide, Thailand", times_no))
+
+# Load current emissions: PM2.5
+current_emissions <- brick(woffile, varname="pm2p5fire")
+labels_current <- substr(names(current_emissions), 7, 11)
+aoi <- as(extent(gfas_data_mean), "SpatialPolygons")
+current_area <- raster::area(current_emissions) * 1000000 # in m2
+current <- raster::mask(current_emissions * current_area, aoi)
+current <- mask(current, country)
+
+# Find indices in common
+idx <- labels_current
+
+# Compute sum over the area
+current_sum <- cellStats(current, sum) * 86400 * 1E-9
+
+jpeg("pm25-month.jpg")
+barplot(current_sum, names.arg=idx, main=paste("Wildfire flux of Particulate Matter PM2.5, Thailand", times_no))
+
 
 # Create a map of emissions for the first day of the month
 #Emissions <- current[[1]]
