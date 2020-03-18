3d183715ee74868d75b0e3690c5b58c259cfba6d
Marko <manzikki@gmail.com>
2020-03-18 08:14:09 +0000

    These scripts now download and process multiple variables

---
+++ month.py
@@ -0,0 +1,54 @@
+#This script downloads last month's "wildfire overall flux of burned carbon"
+#Optional parameters: month year <- will download this
+#Marko Niinimaki 2020
+#!/usr/bin/env python
+from datetime import datetime
+from calendar import monthrange
+from ecmwfapi import ECMWFDataServer
+import sys
+
+def last_day_of_month(date_value):
+    return date_value.replace(day = monthrange(date_value.year, date_value.month)[1])
+
+lastmonth = datetime.now().month - 1
+year = datetime.now().year
+
+if lastmonth == 0:
+    lastmonth = 12
+    year = year - 1
+
+
+if len(sys.argv) == 3:
+    lastmonth = int(sys.argv[1])
+    year = int(sys.argv[2])
+
+mdate = datetime(year=year, month=lastmonth, day=1).date()
+lastday = last_day_of_month(mdate)
+
+monthstr = str(lastmonth)
+if lastmonth < 10:
+    monthstr = "0"+monthstr
+
+dstr = str(year)+"-"+monthstr+"-01/to/"+str(lastday)
+
+print(dstr)
+
+
+#!/usr/bin/env python
+from ecmwfapi import ECMWFDataServer
+server = ECMWFDataServer()
+server.retrieve({
+    "class": "mc",
+    "dataset": "cams_gfas",
+    "date": dstr,
+    "expver": "0001",
+    "levtype": "sfc",
+    "param": "80.210/81.210/87.210/88.210/92.210/99.210",
+#    "param": "92.210",
+    "step": "0-24",
+    "format"    : "netcdf",
+    "stream": "gfas",
+    "time": "00:00:00",
+    "type": "ga",
+    "target": str(year)+"-"+monthstr+"-01wof.nc",
+})
