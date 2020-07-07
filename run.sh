cae9244c36e76830ce97e0cf16b13ab0f95f4689
Marko <manzikki@gmail.com>
2020-07-07 11:41:27 +0000

    daily renames

--- run.sh
+++ run.sh
@@ -6,3 +6,23 @@ python day.py
 
 Rscript day.r 202?-??-??.nc
 
+j=1
+for i in `ls -r 202?-??-??pm25.jpg | head -7`
+do
+    cp $i $j.pm25.jpg
+    ((j++))
+done
+
+j=1
+for i in `ls -r 202?-??-??frp.jpg | head -7`
+do
+    cp $i $j.frp.jpg
+    ((j++))
+done
+
+j=1
+for i in `ls -r 202?-??-??oc.jpg | head -7`
+do
+    cp $i $j.oc.jpg
+    ((j++))
+done
