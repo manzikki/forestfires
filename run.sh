9c74642c9bf5c1666215202fc4ca5a12d5a99a35
Marko <manzikki@gmail.com>
2020-10-22 05:36:47 +0000

    trim images

--- run.sh
+++ run.sh
@@ -2,11 +2,7 @@
 cd "$(dirname "$0")"
 rm -f 202?-??-??.nc
 
-python day.py
-
-Rscript day.r 202?-??-??.nc
-Rscript day-thaionly.r 202?-??-??.nc
-
+#get this month's nc file
 python thismonth.py
 
 year=`date +"%Y"`
@@ -14,5 +10,24 @@ month=`date +"%m"`
 
 if [ -f $year-$month.nc ]
 then
+    #we got it? run the script that builds the bar charts
     Rscript month-simple.r $year-$month.nc
+    Rscript  Extract_ECWMF_vars_SEAdaily.R $year-$month.nc
+else
+    #the month has changed so the data is from the previous month
+    echo foo > /dev/null
+    python month.py
+    #take the last changed month file
+    lastfile=`ls -tr 2020-??.nc | tail -1`
+    Rscript month-simple.r $lastfile
+    Rscript  Extract_ECWMF_vars_SEAdaily.R $lastfile
 fi
+#trim images
+for i in *.jpg
+do
+    /usr/bin/convert -trim $i o.jpg
+    if [ -f o.jpg ]
+    then
+        mv o.jpg $i
+    fi
+done
