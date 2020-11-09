2adf6ab1ccf09edcc8658cd2df321cee348111e7
Marko <manzikki@gmail.com>
2020-11-09 11:47:19 +0000

    moved outdated script

--- variants/make-thai-frp-map.r
+++ variants/make-thai-frp-map.r
@@ -1,6 +1,6 @@
 # Creates a map about forest fire FRP in Thailand
 # Call by: Rscript work.r frpfile.nc
-# Outputfiles: same name as input but jpg instead of nc
+# Outputfiles: same name as input but thaifrp.jpg instead of nc
 # Based on an example by Claudia Vitolo
 # Marko Niinimaki, niinimakim@webster.ac.th 2020
 ### PACKAGES ###
@@ -38,7 +38,7 @@ seabox <- as(raster::extent(89.25,110.38,1.89,28.84), "SpatialPolygons")
 proj4string(seabox) <- CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
 
 # Load global FRP layer and rotate longitude to be in the range [-180, +180]
-frp <- raster::rotate(raster::brick(frpfile))
+frp <- raster::rotate(raster::brick(frpfile, varname="frpfire"))
 
 # Crop frp and pm2p5 over SEA
 frp_sea <- raster::crop(frp, seabox)
@@ -46,7 +46,7 @@ frp_sea <- raster::crop(frp, seabox)
 # Calculate the area of each cell of raster, applies a correction for latitude -> m2
 area_raster <- raster::area(frp_sea[[1]])*1000000
 
-mydate <- str_replace(frpfile, "frp.nc", "")
+mydate <- str_replace(frpfile, ".nc", "")
 mainlabel = paste("FRP in W/m2", mydate)
 
 # Plot the FRP to frp.jpg
@@ -54,10 +54,11 @@ jpeg("frp.jpg")
 
 plot(frp_sea, main = mainlabel,
      xlab = "Lon", ylab = "Lat")
-plot(tha, add= TRUE)
+#breaks=c(0.0,0.2,0.4,0.6,0.8,1.0,2.0,3.0))
+plot(tha, add = TRUE)
 
 dev.off()
 
-oname <- str_replace(frpfile, ".nc", ".jpg")
+oname <- str_replace(frpfile, ".nc", "thaifrp.jpg")
 file.copy("frp.jpg", oname)
 
