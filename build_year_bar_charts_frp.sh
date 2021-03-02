c915b97026074501a50dc26cb5e66b63c33d8e9f
Marko <manzikki@gmail.com>
2021-03-02 11:37:44 +0700

    by-year FRP graphs

---
+++ build_year_bar_charts_frp.sh
@@ -0,0 +1,13 @@
+Rscript year_bar_charts_frp.r jan.frp.csv January
+Rscript year_bar_charts_frp.r feb.frp.csv February
+Rscript year_bar_charts_frp.r mar.frp.csv March
+Rscript year_bar_charts_frp.r apr.frp.csv April
+Rscript year_bar_charts_frp.r may.frp.csv May
+Rscript year_bar_charts_frp.r jun.frp.csv June
+Rscript year_bar_charts_frp.r jul.frp.csv July
+Rscript year_bar_charts_frp.r aug.frp.csv August
+Rscript year_bar_charts_frp.r sep.frp.csv September
+Rscript year_bar_charts_frp.r oct.frp.csv October
+Rscript year_bar_charts_frp.r nov.frp.csv November
+Rscript year_bar_charts_frp.r dec.frp.csv December
+
