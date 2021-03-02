3d5b4e737fd3be112084d429c32c661f3a696cdd
Marko <manzikki@gmail.com>
2021-03-02 11:19:09 +0700

    build script

---
+++ build_year_bar_charts_pm25.sh
@@ -0,0 +1,13 @@
+Rscript year_bar_charts_pm25.r jan.pm25.csv January
+Rscript year_bar_charts_pm25.r feb.pm25.csv February
+Rscript year_bar_charts_pm25.r mar.pm25.csv March
+Rscript year_bar_charts_pm25.r apr.pm25.csv April
+Rscript year_bar_charts_pm25.r may.pm25.csv May
+Rscript year_bar_charts_pm25.r jun.pm25.csv June
+Rscript year_bar_charts_pm25.r jul.pm25.csv July
+Rscript year_bar_charts_pm25.r aug.pm25.csv August
+Rscript year_bar_charts_pm25.r sep.pm25.csv September
+Rscript year_bar_charts_pm25.r oct.pm25.csv October
+Rscript year_bar_charts_pm25.r nov.pm25.csv November
+Rscript year_bar_charts_pm25.r dec.pm25.csv December
+
