756afcc331f040ce269d884763b8deb8741e1d2a
Marko <manzikki@gmail.com>
2021-04-07 09:10:21 +0000

    variable fix pm->co2

--- Extract_ECWMF_PM25a_SEA_countries_daily.R
+++ Extract_ECWMF_PM25a_SEA_countries_daily.R
@@ -1,8 +1,8 @@
 #By Dr. Praphatsorn Punsompong 2020.
 #Small adjustments by Marko Niinimaki.
 #This program gets a NetCFD file from the command line and creates maps of upper SEA for each of the
-#dates for variables frp, co2fire and pm25fire that should be found in the NetCDF file.
-#It produces files datefrp.jpg, dateco2.jpg, datepm25.jpg
+#dates for variable pm25 that should be found in the NetCDF file.
+#It produces file: datepm25.jpg
 
 #For truncating very long label numbers
 scalef <- function(x) sprintf("%.2f", x)
@@ -79,7 +79,7 @@ library(stringr)
 #get the parameter
 args = commandArgs(trailingOnly=TRUE)
 if (length(args) < 1) {
-  stop("Parameters needed: monthly file in the form 2020-XX.nc", call.=FALSE)
+  stop("Parameters needed: monthly file in the form 2020-XXpm25.nc", call.=FALSE)
 }
 aNCfile = args[1]
 
@@ -90,54 +90,39 @@ aDataDate = str_replace(aNCfile, ".nc", "")
 seabox <- as(raster::extent(89.25,110.38,1.89,28.84), "SpatialPolygons")
 
 #extraction
-aFRP_data = brick(aNCfile, var="frpfire")
 aPM25_data = brick(aNCfile, var="pm2p5fire")
-aCO2_data = brick(aNCfile, var="co2fire")
+#dates
+tatt = attributes(aPM25_data)[2]
+firstdate = names(tatt$data)[1]
+print(firstdate)
+myyear = substr(firstdate, 2, 5)
+print(myyear)
+mymon = substr(firstdate, 7, 8)
+print(mymon)
 
-crs(aFRP_data) = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
 crs(aPM25_data) = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
-crs(aCO2_data) = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
-
-aFRP_SEA = mask(aFRP_data,seabox)
 
 aPM25_SEA = mask(aPM25_data,seabox)
 aPM25_SEAmg = aPM25_SEA * 1000000000 #mg/m^2
 
-aCO2_SEA = mask(aCO2_data,seabox)
-aCO2_SEAmg = aCO2_SEA * 1000000000 #mg/m^2
-
 #plot daily maps
-aNumDay = nlayers(aFRP_SEA) #number of days in Month
+aNumDay = nlayers(aPM25_SEA) #number of days in Month
 
 for (aDay in (1:aNumDay)) {
   aDate = ifelse(aDay < 10,paste0("0",aDay),aDay)
-  aMapDate = paste0(aDataDate,".",aDate)
-  aMapDate = str_replace(aMapDate,"-",".")
-  aFileDate = paste0(aDataDate,"-",aDate)
+  aMapDate = paste0(myyear,".",mymon,".",aDate)
+  aFileDate = paste0(myyear,"-",mymon,"-",aDate)
   #print(aMapDate)
   print(aFileDate)
 
-  atR = subset(aFRP_SEA,aDay) #extract time:day in nc data
-  aFRP_D = as.data.frame(atR, xy = TRUE) #change raster to dataframe
   atR = subset(aPM25_SEAmg,aDay)
   aPM25_D = as.data.frame(atR, xy = TRUE)
-  atR = subset(aCO2_SEAmg,aDay)
-  aCO2_D = as.data.frame(atR, xy = TRUE)
 
-  names(aFRP_D)[3]="FRP"
   names(aPM25_D)[3]="PM25"
-  names(aCO2_D)[3]="CO2"
 
-  summary(aFRP_D)
+  summary(aPM25_D)
 
-  aMaxF = max(aFRP_D$FRP, na.rm = TRUE)
-  FRP_breaks = seq(0,aMaxF,0.2)
-  if (aMaxF > 2) { FRP_breaks = seq(0,aMaxF,0.5) }
-  if (aMaxF > 6) { FRP_breaks = seq(0,aMaxF,1) }
-  aMaxF = ifelse(aMaxF < 1,1,aMaxF)
   #print(FRP_breaks)
-  jpeg(paste0(aFileDate , "frp.jpg"), width = 1442 , height = 1442 , res = 200)
-  draw_map_with_data("FRP in Upper SEA", "FRP (W/m2)", aMaxF, aFRP_D, FRP_breaks, FRP_breaks, aFRP_D$FRP)
 
   aMaxP = max(aPM25_D$PM25, na.rm = TRUE)
   #print("Max PM2.5")
@@ -146,21 +131,12 @@ for (aDay in (1:aNumDay)) {
   #these breaks should be hardcoded for consistency
   PM25_breaks = seq(0,aMaxP,length.out=5)
 
-  jpeg(paste0(aFileDate , "pm25.jpg"), width = 1442 , height = 1442 , res = 200)
+  jpeg(paste0(aFileDate , "pm25a.jpg"), width = 1442 , height = 1442 , res = 200)
   PM25_labels = scalef(PM25_breaks)
 
-  draw_map_with_data("PM 2.5 from fires", expression(paste("PM 2.5 mg m"^"-2","s"^"-1")), 
+  draw_map_with_data("PM 2.5 emissions in general", expression(paste("PM 2.5 mg m"^"-2","s"^"-1")), 
                       aMaxP, aPM25_D, PM25_breaks, PM25_labels, aPM25_D$PM25)
 
-  aMaxC = max(aCO2_D$CO2, na.rm = TRUE)
-  #print("Max CO2")
-  #print(aMaxC) #3.841185e-09
-  #aMaxC = ifelse(aMaxC < 1,1,aMaxC)
-  CO2_breaks = seq(0,aMaxC,length.out=10)
-  CO2_labels = scalef(CO2_breaks)
-
-  jpeg(paste0(aFileDate , "co2.jpg"), width = 1442 , height = 1442 , res = 200)
-  draw_map_with_data("Fire CO2 in Upper SEA", expression(paste("CO2 mg m"^"-2","s"^"-1")), aMaxC, aCO2_D, CO2_breaks, CO2_labels, aCO2_D$CO2)
 
 }
 
