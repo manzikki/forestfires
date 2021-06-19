9a5104b46926814985fe7debf02fd5bec595bd60
Marko <manzikki@gmail.com>
2021-06-19 09:00:12 +0000

    script name change

--- hourly/run-hourly.sh
+++ hourly/run-hourly.sh
@@ -13,27 +13,27 @@ recentf=`ls *frp.nc | tail -1`
 recentp=`ls *pm25.nc | tail -1`
 recentc=`ls *co2.nc | tail -1`
 
-Rscript daily_frp_by_country.r $recentf THA
-Rscript daily_frp_by_country.r $recentf LAO
-Rscript daily_frp_by_country.r $recentf KHM
-Rscript daily_frp_by_country.r $recentf MMR
-Rscript daily_frp_by_country.r $recentf VNM
+Rscript map_daily_frp_by_country.r $recentf THA
+Rscript map_daily_frp_by_country.r $recentf LAO
+Rscript map_daily_frp_by_country.r $recentf KHM
+Rscript map_daily_frp_by_country.r $recentf MMR
+Rscript map_daily_frp_by_country.r $recentf VNM
 
-Rscript daily_pm25_by_country.r $recentp THA
-Rscript daily_pm25_by_country.r $recentp LAO
-Rscript daily_pm25_by_country.r $recentp KHM
-Rscript daily_pm25_by_country.r $recentp MMR
-Rscript daily_pm25_by_country.r $recentp VNM
+Rscript map_daily_pm25_by_country.r $recentp THA
+Rscript map_daily_pm25_by_country.r $recentp LAO
+Rscript map_daily_pm25_by_country.r $recentp KHM
+Rscript map_daily_pm25_by_country.r $recentp MMR
+Rscript map_daily_pm25_by_country.r $recentp VNM
 
-Rscript daily_co2_by_country.r $recentc THA
-Rscript daily_co2_by_country.r $recentc LAO
-Rscript daily_co2_by_country.r $recentc KHM
-Rscript daily_co2_by_country.r $recentc MMR
-Rscript daily_co2_by_country.r $recentc VNM
+Rscript map_daily_co2_by_country.r $recentc THA
+Rscript map_daily_co2_by_country.r $recentc LAO
+Rscript map_daily_co2_by_country.r $recentc KHM
+Rscript map_daily_co2_by_country.r $recentc MMR
+Rscript map_daily_co2_by_country.r $recentc VNM
 
-Rscript daily_frp_sea.r $recentf
-Rscript daily_pm25_sea.r $recentp
-Rscript daily_co2_sea.r $recentc
+Rscript map_daily_frp_sea.r $recentf
+Rscript map_daily_pm25_sea.r $recentp
+Rscript map_daily_co2_sea.r $recentc
 
 recenty=`ls *pm25.nc | tail -1 | sed 's/-.*//'`
 recentm=`ls *pm25.nc | tail -1 | sed 's/^....-//' | sed 's/-.*//'`
