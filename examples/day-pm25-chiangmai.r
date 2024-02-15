1de8208ee56e8560632d07a50d4ef8f821815fbe
M <marko.n@chula.ac.th>
2024-02-15 11:31:46 +0700

    mean, not sum

--- examples/day-pm25-chiangmai.r
+++ examples/day-pm25-chiangmai.r
@@ -1,12 +1,12 @@
-# Print the daily value of sum of pm2.5 in the Chiang Mai province
+# Print the daily value of avg pm2.5 in the Chiang Mai province
 # Based on ECMWF examples, tuning by Marko Niinimaki marko.n@chula.ac.th 2024
-# install.packages(c("raster", "mapview", "ncfd4"))
+# Use parameter: an netcdf file containing one day of data, variable pm2p5fire
+# install.packages(c("raster"))
 
 library("raster")
-# library("mapview")
-library("ncdf4")
+#library("ncdf4")
 #library("ncdf.tools")
-library("stringr")
+#library("stringr")
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -14,22 +14,22 @@ if (length(args) < 1) {
   stop("Parameters needed: dayfile.nc", call.=FALSE)
 }
 
-woffile = args[1]
+pmfile = args[1]
 
 country <- raster::getData(name = "GADM", country = "Thailand", level = 1)
 chiangmai <- country[country$NAME1=="ChiangMai"]
 
 
 # Load current emissions: PM2.5
-current_emissions <- brick(woffile, varname="pm2p5fire")
+current_emissions <- brick(pmfile, varname="pm2p5fire")
 #crop to include chiangmai only
 chiangmai_em = crop(current_emissions, chiangmai)
-#not sure about this. why would we multiply because in the sum we divide
+#not sure about this. why would we multiply because next we divide
 current <- raster::area(chiangmai_em) # * 1000000 # in m2
 
-# Compute sum over the area
-current_sum <- cellStats(current, sum) * 86400 * 1E-3
-print(current_sum)
+# Compute mean over the area
+current_mean <- cellStats(current, mean) * 86400 * 1E-3
+print(current_mean)
 
 #current <- raster::mask(current_emissions * current_area, chiangmai)
 #current <- mask(current, chiangmai)
